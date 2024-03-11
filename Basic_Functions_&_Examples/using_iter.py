8507f1bf49936cb31a3a7f037dab2aed95943e5e
Ayush Siloiya <85118674+ayushsiloiya619@users.noreply.github.com>
2024-03-11 09:16:56 +0530

    using_iter

---
+++ Basic_Functions_&_Examples/using_iter.py
@@ -0,0 +1,12 @@
+color=['Red','Tasty','Big']
+fruits=['Apple','Banana','Orange']
+for x in color:
+    for y in fruits:
+        print(x,y)
+strings="Ayush"
+myitr=iter(strings)
+print(next(myitr))
+print(next(myitr))
+print(next(myitr))
+print(next(myitr))
+print(next(myitr))
